775ae42592c872afd30ee7f71ec8463c9651c1f9
Bobby Lucero <bobbylucero1@gmail.com>
2024-09-10 23:20:43 -0400

    Modifications

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftRaylibTemplate",
     products: [
-        .executable(name: "SwiftRaylibTemplate", targets: ["SwiftRaylibTemplate"])
+        
     ],
     dependencies: [
         .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master"),
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftRaylibTemplate",
+        .executableTarget(name: "SwiftRaylibTemplate",
             dependencies: [
                 .product(name: "Raylib", package: "Raylib"),
             ]
